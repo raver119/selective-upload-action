95bdfe8fd6493b9682d928563c1392a1aa308ced
Vyacheslav Kokorin <raver119@pm.me>
2023-03-26 15:21:57 +0200

    env var

--- src/tests/Upload.test.ts
+++ src/tests/Upload.test.ts
@@ -25,7 +25,7 @@ afterAll(() => {
 
 describe("Upload", () => {
   const client = new S3Client({
-    endpoint: "http://localhost:9090",
+    endpoint: `http://${process.env.S3_ENDPOINT ?? "localhost"}:9090`,
     region: "any",
     credentials: {
       accessKeyId: uuid(),
